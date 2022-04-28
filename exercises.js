fa9f4b63da3a9f753243fcc36e3e3e72a55b52e9
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-04-27 18:26:49 -0600

    Complete Part 1

--- exercises.js
+++ exercises.js
@@ -6,85 +6,105 @@ const album1 = {
   title: "Talking Heads",
   albumDetails: {
     released: new Date("September 16, 1977"),
-    label:    "Sire",
-    formats:  ["LP"]
-  }
-}  
+    label: "Sire",
+    formats: ["LP"],
+  },
+};
 
 // 1. Retrieve the string "Sire" from album1, and save it in a sensibly named
 //    variable.
+const albumLabel = album1.albumDetails.label;
+console.log(albumLabel);
 
 // 2. Change the title of album1 from "Talking Heads" to "Talking Heads: 77"
+album1.title += ": 77";
+console.log(album1.title);
 
 const album2 = {
   title: "More Songs About Buildings and Food",
   albumDetails: {
     released: new Date("July 14, 1978"),
-    label:    "Sire",
-    formats:  ["LP", "8-track"]
-  }
-}  
+    label: "Sire",
+    formats: ["LP", "8-track"],
+  },
+};
 
 const album3 = {
   title: "Fear of Music",
   albumDetails: {
     released: "August 3, 1979",
-    label:    "Sire",
-    formats:  ["Cassette"]
-  }
-}  
+    label: "Sire",
+    formats: ["Cassette"],
+  },
+};
 
 // 3. Access album2's formats array and use an array method to add "LP" to
 //    album3's formats
 // Check out the Array.push method!
 
+album3.albumDetails.formats.push(album2.albumDetails.formats[0]);
+console.log(album3.albumDetails.formats);
+
 // 4. Change the release date of album3 from a string into a Date object
 // Look ahead to album4 for a clue!
+album3.albumDetails.released = new Date(album3.albumDetails.released);
+console.log(album3.albumDetails.released);
 
 const album4 = {
   title: "Remain in Light",
   albumDetails: {
     released: new Date("October 8, 1980"),
-    formats: ["Cassette", "LP"]
-  }
-}  
+    formats: ["Cassette", "LP"],
+  },
+};
 
 // 5. Add the label "Sire" to album4's details
+
+album4.albumDetails.label = "Sire";
+console.log(album4.albumDetails.label);
 
 const album5 = {
   title: "Speaking in Tongues",
   albumDetails: {
     released: new Date("May 31, 1983"),
-    label:    "Sire"
-  }
-}  
+    label: "Sire",
+  },
+};
 
 // 6. Add a 'formats' array to album 5 and add "CD", "Cassette", and "LP"
+album5.albumDetails.formats = ["CD", "Cassette", "LP"];
+console.log(album5.albumDetails.formats);
 
 const album6 = {
   title: "Little Creatures",
   albumDetails: {
     released: new Date("June 10, 1985"),
-    labels:   ["Sire", "emi"],
-    formats:  ["CD", "cassette", "LP"]
-  }
-}  
+    labels: ["Sire", "emi"],
+    formats: ["CD", "cassette", "LP"],
+  },
+};
 
 // 7. Make the label "emi" in album6 all uppercase
 // google how to make a string uppercase in js!
+
+album6.albumDetails.labels[1] = album6.albumDetails.labels[1].toUpperCase();
+console.log(album6.albumDetails.labels[1]);
 
 const album7 = {
   title: "True Stories",
   albumDetails: {
     released: new Date("October 7, 1986"),
-    labels:   "Sire, EMI",
-    formats:  ["CD", "cassette", "LP"]
-  }
-}  
+    labels: "Sire, EMI",
+    formats: ["CD", "cassette", "LP"],
+  },
+};
 
 // 8. Convert album7's 'labels' property from the string value
 //    "Sire, EMI" into the array: ["Sire", "EMI"]
 // google js array split!
+
+album7.albumDetails.labels = album7.albumDetails.labels.split(", ");
+console.log(album7.albumDetails.labels);
 
 /////////////////////////////////////////////////////
 // Part 2: More Tasks About Datatypes and Structures
@@ -94,10 +114,10 @@ const album8 = {
   title: "Naked",
   albumDetails: {
     released: new Date("March 15, 1988"),
-    labels:    ["Sire", "EMI"],
-    formats:  ["CD", "cassette", "LP"]
-  }
-}  
+    labels: ["Sire", "EMI"],
+    formats: ["CD", "cassette", "LP"],
+  },
+};
 
 const talkingHeadsAlbums = [
   album1,
@@ -107,8 +127,8 @@ const talkingHeadsAlbums = [
   album5,
   album6,
   album7,
-  album8
-]  
+  album8,
+];
 
 // 1. Create an object literal called `band`.
 
@@ -183,13 +203,13 @@ const talkingHeadsAlbums = [
 //      section: "right",
 //      type: "premium",
 //      seats: 1
-//    }  
+//    }
 //    const ticket1 = {
 //      name: "Newt Gingrich",
 //      section: "center",
 //      type: "standard",
 //      seats: 4
-//    }  
+//    }
 //
 //    Messages:
 //    "Welcome, Madeline Albright! You may sit anywhere in the first 3 rows of the right section."
@@ -197,13 +217,18 @@ const talkingHeadsAlbums = [
 //     Please be sure to leave no seats between you."
 
 const ticketSections = [
-  {name: "Boutros Boutros-Ghali", section: "center", type: "premium",  seats: 1},
-  {name: "Ann Richards",          section: "left",   type: "premium",  seats: 2},
-  {name: "George Will",           section: "left",   type: "standard", seats: 2},
-  {name: "Marion Barry",          section: "center", type: "standard", seats: 8},
-  {name: "Warren Christopher",    section: "right",  type: "standard", seats: 1},
-  {name: "Bob Dole",              section: "center", type: "premium",  seats: 3}
-]  
+  {
+    name: "Boutros Boutros-Ghali",
+    section: "center",
+    type: "premium",
+    seats: 1,
+  },
+  { name: "Ann Richards", section: "left", type: "premium", seats: 2 },
+  { name: "George Will", section: "left", type: "standard", seats: 2 },
+  { name: "Marion Barry", section: "center", type: "standard", seats: 8 },
+  { name: "Warren Christopher", section: "right", type: "standard", seats: 1 },
+  { name: "Bob Dole", section: "center", type: "premium", seats: 3 },
+];
 
 // 2. There is a concert at the LA County Fairgrounds by the Southland's
 //    hottest Talking Heads tribute band for zombie afficianados,
@@ -233,14 +258,14 @@ const ticketSections = [
 //    - {amount: 50.00, discount: true,  zombie: true}   => "STANDARD $20 DRINKS"
 
 const tickets = [
-  {amount: 50.00, discount: false, zombie: true},
-  {amount: 60.00, discount: true,  zombie: false},
-  {amount: 50.00},
-  {amount: 65.00, discount: true,  zombie: true},
-  {amount: 90.00, discount: false},
-  {amount: 50.00, discount: true,  zombie: false},
-  {amount: 50.00, zombie:   true},
-  {amount: 80.00, discount: true},
-  {amount: 90.00},
-  {amount: 50.00, discount: true}
-]  
+  { amount: 50.0, discount: false, zombie: true },
+  { amount: 60.0, discount: true, zombie: false },
+  { amount: 50.0 },
+  { amount: 65.0, discount: true, zombie: true },
+  { amount: 90.0, discount: false },
+  { amount: 50.0, discount: true, zombie: false },
+  { amount: 50.0, zombie: true },
+  { amount: 80.0, discount: true },
+  { amount: 90.0 },
+  { amount: 50.0, discount: true },
+];
